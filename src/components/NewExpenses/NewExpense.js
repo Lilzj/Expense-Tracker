83e0294bc2961104ba9ebaf0174e38ae4b25fb17
Gbogodor Joshua <joshuagbogodor@gmail.com>
2021-09-22 20:37:08 +0100

    Added a handler to be able to save new added expense

--- src/components/NewExpenses/NewExpense.js
+++ src/components/NewExpenses/NewExpense.js
@@ -4,8 +4,17 @@ import './NewExpense.css'
 
 const NewExpense = () =>
 {
+    const saveNewExpenseHandler = (newExpenses) => 
+    {
+        const expenseData = 
+        {
+            ...newExpenses,
+            id: (Math.random() + 1).toString()
+        }
+    }
+
     return <div className="new-expense">
-       <ExpenseForm />
+       <ExpenseForm  onNewExpenseSave = {saveNewExpenseHandler} />
     </div>
 }
 
